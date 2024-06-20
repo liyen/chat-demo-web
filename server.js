94759c0ad5ce9831f8bb9b277667d203174b8e35
liyen <yoi022.rita@gmail.com>
2024-06-20 09:57:00 +0000

    Render index.html page

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'view')));
+
+app.get( "/", ( req, res ) => {
+  // send the HTML file in the API response
+  res.sendFile( path.join( __dirname + "/view/index.html" ));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
